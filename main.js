efef95c738101f439e1a39d204808043948fd496
Santiago <san.x0000@gmail.com>
2022-12-25 22:39:52 -0300

    layout concluido

--- main.js
+++ main.js
@@ -1,39 +1,11 @@
-function tocaSom (seletorAudio) {
-    const elemento = document.querySelector(seletorAudio);
-
-    if (elemento && elemento.localName === 'audio') {
-        elemento.play();
-    }
-    else {
-        //alert('Elemento não encontrado');
-        console.log('Elemento não encontrado ou seletor inválido');
-    }
-
+function kenshinSom01 () {
+    document.querySelector('#som_kenshin_01').play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
-
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
-
-    tecla.onkeydown = function (evento) {
-
-        if (evento.code === 'Space' || evento.code === 'Enter') {
-            tecla.classList.add('ativa');
-        }
-
-    }
-
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
-
+function frontierSom01 () {
+    document.querySelector('#som_frontier_01').play();
 }
+
+
+
+//document.querySelector('.kenshin').onclick = tocaSomPom;
