797a990457961103d85e40daf31fdb67566d13b7
Santiago Barreto <115950745+SanX0000@users.noreply.github.com>
2023-01-01 22:17:14 -0300

    todas musicas adicionadas

--- Nova pasta/main.js
+++
@@ -1,7 +0,0 @@
-function tocaKenshin() {
-    document.querySelector('.samurai').play();
-}
-
-const kenshinSounds = document.querySelectorAll('.samurai');
-
-for (let contador = 0; contador < kenshinSounds.length; contador++);
--- listaAnimes.js
+++ listaAnimes.js
@@ -9,20 +9,104 @@ export const listaAnimes = [
             "kenshin06.mp3",
         ]
     },
+
     {
-        nome: 'shingeki',
+        nome: 'frontier',
         musicas: [
-            "kenshin04.mp3",
-            "kenshin05.mp3",
+            "macross01.mp3",
+            "macross02.mp3",
+            "macross03.mp3",
+            "macross04.mp3",
+            "macross05.mp3",
+            "macross06.mp3",
+            "macross07.mp3"
+        ]
+    },
+
+    {
+        nome: "titan",
+        musicas: [
+            "titan01.mp3",
+            "titan02.mp3",
+            "titan03.mp3",
         ]
     },
 
     {
         nome: 'naruto',
         musicas: [
-            "kenshin04.mp3",
-            "kenshin05.mp3"
+            "naruto01.mp3",
+            "naruto02.mp3",
+            "naruto03.mp3",
+            "naruto04.mp3",
+            "naruto05.mp3",
+            "naruto06.mp3",
+            "naruto07.mp3",
+            "naruto08.mp3",
+            "naruto09.mp3",
+            "naruto10.mp3",
         ]
     },
 
+    {
+        nome: 'gits',
+        musicas: [
+            "gits01.mp3",
+            "gits02.mp3",
+            "gits03.mp3",
+        ]
+    },
+
+    {
+        nome: 'code',
+        musicas: [
+            "code01.mp3",
+            "code02.mp3",
+            "code03.mp3",
+            "code04.mp3",
+            "code05.mp3",
+            "code06.mp3",
+        ]
+    },
+
+    {
+        nome: 'saint',
+        musicas: [
+            "saint01.mp3",
+            "saint02.mp3",
+            "saint03.mp3",
+            "saint04.mp3",
+            "saint05.mp3",
+            "saint06.mp3",
+            "saint07.mp3",
+            "saint08.mp3",
+            "saint09.mp3",
+            "saint10.mp3",
+        ]
+    },
+
+    {
+        nome: 'gundam',
+        musicas: [
+            "gundam01.mp3",
+            "gundam02.mp3",
+            "gundam03.mp3",
+            "gundam04.mp3",
+            "gundam05.mp3",
+            "gundam06.mp3",
+            "gundam07.mp3",
+        ]
+    },
+
+    {
+        nome: 'shura',
+        musicas: [
+            "shura01.mp3",
+            "shura02.mp3",
+            "shura03.mp3",
+            "shura04.mp3",
+            "shura05.mp3",
+            "shura06.mp3",
+        ]
+    },
 ]
